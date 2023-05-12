59e83d361242a3bd5b3d9cae3b0ba1f34716459c
Tafie <tafadzwachijarira96@gmail.com>
2023-05-12 14:19:58 +0200

    checkbox

--- DEP.js
+++ DEP.js
@@ -1,18 +1,19 @@
-function checkElegibility() {
+
+const checkbox = document.getElementById("myCheckbox");
+
+            checkbox.addEventListener("change",function checkElegibility()) {
             let age= document.getElementById("age").value;
             if (age>=65){
                 document.getElementById("output").innerHTML =
                 "Congradulations you qualify for senior Discount";
-            } else if (age >=18 && isMember.checked){
+            } else if (Number(age) >=18 && checkElegibility().checked){
                 document.getElementById("output").innerHTML =
                 "Congradulation you are Eligible for Member Discount";
             } else {
                 document.getElementById("output").innerHTML =
                 "Sorry you are not Eligible For Discount";
             }
+            //prevent the default submission behavior, which is to refresh the page.
+            event.preventDefault();
+             
         }
-        const form = document.getElementById("form");
-
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-          });
