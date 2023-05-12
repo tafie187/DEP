cbf3462a13c325b6591d005e2288208311eef42e
Tafie <tafadzwachijarira96@gmail.com>
2023-05-12 14:42:59 +0200

    FINAL CHANGES

--- DEP.js
+++ DEP.js
@@ -1,19 +1,15 @@
-
-const checkbox = document.getElementById("myCheckbox");
-
-            checkbox.addEventListener("change",function checkElegibility()) {
-            let age= document.getElementById("age").value;
-            if (age>=65){
-                document.getElementById("output").innerHTML =
-                "Congradulations you qualify for senior Discount";
-            } else if (Number(age) >=18 && checkElegibility().checked){
-                document.getElementById("output").innerHTML =
-                "Congradulation you are Eligible for Member Discount";
-            } else {
-                document.getElementById("output").innerHTML =
-                "Sorry you are not Eligible For Discount";
-            }
-            //prevent the default submission behavior, which is to refresh the page.
-            event.preventDefault();
-             
-        }
+function checkEligibility() {
+    let age = document.getElementById("age").value;
+    let checkbox = document.getElementById("myCheckbox");
+  
+    if (age >= 65) {
+      document.getElementById("output").innerHTML = "Congratulations! You qualify for a senior discount.";
+    } else if (Number(age) >= 18 && checkbox.checked) {
+      document.getElementById("output").innerHTML = "Congratulations! You are eligible for a member discount.";
+    } else {
+      document.getElementById("output").innerHTML = "Sorry, you are not eligible for a discount.";
+    }
+  
+    // Prevent the default submission behavior, which is to refresh the page.
+    event.preventDefault();
+  }
